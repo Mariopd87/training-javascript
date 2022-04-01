07e4b0ab897f8a09cf11982f1d372d47ffc8d85e
mario <mariopd87@gmail.com>
2022-04-01 13:02:18 +0200

    start querySelector

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav");
+console.log(nav);
+let active = document.querySelector(".active");
+console.log(active);
+let paragraph = document.querySelector("#first");
+console.log(paragraph);
+
+let headersAndParagraphs = document.querySelectorAll('h1,p')
+console.log(headersAndParagraphs[1]);
