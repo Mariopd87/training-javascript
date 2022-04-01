5e191bc7d2159f68febf7efc760c58d36ecae32c
mario <mariopd87@gmail.com>
2022-04-01 20:47:52 +0200

    example DOM

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
-let nav = document.querySelector("nav");
-console.log(nav);
-let active = document.querySelector(".active");
-console.log(active);
-let paragraph = document.querySelector("#first");
-console.log(paragraph);
+// let nav = document.querySelector("nav");
+// //console.log(nav);
+// let active = document.querySelector(".active");
+// //console.log(active);
+// let paragraph = document.querySelector("#first");
+// //console.log(paragraph);
 
-let headersAndParagraphs = document.querySelectorAll('h1,p')
-console.log(headersAndParagraphs[1]);
+// let headersAndParagraphs = document.querySelectorAll("h1,p")
+// //console.log(headersAndParagraphs[1].innerText);
+
+// // console.log(nav.children[0].children[0].children[0]);
+// // console.log(headersAndParagraphs[0].nextElementSibling.nextElementSibling);
+// // console.log(document.body.children[1]. firstElementChild.lastElementChild.parentElement.parentElement);
+// // console.log(nav.firstElementChild.childElementCount);
+
+// console.log(headersAndParagraphs[0].firstChild);
+// console.log(headersAndParagraphs[0].firstChild.nodeValue);
+//console.log(nav.nodeName);
+
+// let box1 = document.querySelector("section").firstElementChild;
+// let box2 = box1.nextElementSibling;
+// let box3 = box2.nextElementSibling;
+
+// box1.classList.add("dark");
+// box1.classList.remove("dark");
+// box2.classList.add("dissapear")
+// box2.classList.remove("dissapear")
